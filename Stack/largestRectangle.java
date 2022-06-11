84026f5e6f55f2fbc7304e6b96fb75c40c8dd434
SaNcHiT104 <jainsanchit14112002@gmail.com>
2022-06-11 10:05:18 +0530

    ..

---
+++ Stack/largestRectangle.java
@@ -0,0 +1,49 @@
+class largestRectangleArea{
+    public int largestRectangleArea(int[] heights) {
+        int[] nsol = nextSmallerOnleft(heights);
+        int[] nsor = nextSmallerOnRight(heights);
+        int max = 0;
+        for (int i = 0; i < heights.length; i++) {
+            int w = nsor[i] - nsol[i] - 1;
+            int area = heights[i] * w;
+            max = Math.max(area, max);
+
+        }
+        return max;
+    }
+
+    public static int[] nextSmallerOnleft(int[] arr) {
+        int[] ans = new int[arr.length];
+        Stack<Integer> st = new Stack<>();
+        for (int i = arr.length - 1; i >= 0; i--) {
+            while (st.size() != 0 && arr[st.peek()] > arr[i]) {
+                int idx = st.pop();
+                ans[idx] = i;
+            }
+            st.push(i);
+        }
+        while (st.size() != 0) {
+            int idx = st.pop();
+            ans[idx] = -1;
+        }
+        return ans;
+    }
+
+    public static int[] nextSmallerOnRight(int[] arr) {
+        int[] ans = new int[arr.length];
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0; i < arr.length; i++) {
+            while (st.size() != 0 && arr[st.peek()] > arr[i]) {
+                int idx = st.pop();
+                ans[idx] = i;
+            }
+            st.push(i);
+        }
+        while (st.size() != 0) {
+            int idx = st.pop();
+            ans[idx] = arr.length;
+        }
+        return ans;
+    }
+
+}
